b169d9339f30cd53598d7c6504b274e3f3e6efa1
vigu184 <vigneshiyer666666@gmail.com>
2021-04-25 20:53:33 +0530

    Email and password validation set

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,79 +1,113 @@
 import React from "react";
 import { Button, TextField } from "@material-ui/core";
 import { Grid, Paper } from "@material-ui/core";
-import {createMuiTheme} from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const darkTheme = createMuiTheme({
   palette: {
-    type: 'dark',
+    type: "dark",
   },
 });
 
+const EmailRegex = RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,10}$/i);
+const InvalidPasswordRegex = RegExp(
+  /^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$/
+);
 class Login extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      username: "",
+      email: "",
       password: "",
+      errors: {
+        email: "",
+        password: "",
+      },
     };
   }
 
-  handleSubmit = () => {
-    console.log("");
+  validate = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    const errors = this.state.errors;
+    switch (name) {
+      case "email":
+        errors.email = EmailRegex.test(value) ? "" : "Invalid Email";
+        console.log(errors);
+        break;
+      case "password":
+        errors.password = InvalidPasswordRegex.test(value)
+          ? alert(
+              "Password must contain minimum 8 characters with at least 1 Uppercase, 1 Lowercase, 1 Number and 1 special character"
+            )
+          : "";
+        console.log(errors);
+        break;
+      default:
+        break;
+    }
   };
 
   onChange = (event) => {
     const caller = event.target.name;
     const value = event.target.value;
-    this.setState({[caller]: value,});
+    this.setState({ [caller]: value });
   };
 
   render() {
     return (
       <>
-      <Grid container justify="center">
-      <Paper
-          component={Grid}
-          item
-          container
-          alignContent="center"
-          direction="column"
-          md={4}
-          style={{ paddingTop: 100, paddingBottom:20}}
-          className={darkTheme}
-        >
-
-          <div>
-            <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
-              <Grid>
-                <TextField
-                  name="username"
-                  label="Enter User Name"
-                  variant="filled"
-                  value={this.state.username}
-                  onChange={this.onChange}
-                />
-              </Grid>
-              <Grid style={{paddingTop:10}}>
-                <TextField
-                  name="password"
-                  label="Enter Password"
-                  type="password"
-                  variant="filled"
-                  value={this.state.password}
-                  onChange={this.onChange}
-                />
-              </Grid>
-            <Grid style={{paddingTop:10}} container justify="center">
-            <Button color="primary" variant="contained" type="submit">
-              Submit
-            </Button>
-            </Grid>
-            </form>
-            
-          </div>
-        </Paper>
-      </Grid>
+        <Grid container justify="center">
+          <Paper
+            component={Grid}
+            item
+            container
+            variant="outlined"
+            direction="column"
+            alignContent="center"
+            md={4}
+            style={{ paddingTop: 100, paddingBottom: 20 }}
+            className={darkTheme}
+          >
+            <div>
+              <form
+                onSubmit={this.handleSubmit}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  padding: "2em",
+                }}
+              >
+                <Grid>
+                  <TextField
+                    name="email"
+                    label="Enter Email ID"
+                    variant="outlined"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                    onBlur={this.validate}
+                  />
+                </Grid>
+                <Grid style={{ paddingTop: 10 }}>
+                  <TextField
+                    name="password"
+                    label="Enter Password"
+                    type="password"
+                    variant="outlined"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                    onBlur={this.validate}
+                  />
+                </Grid>
+                <Grid style={{ paddingTop: 10 }} container justify="center">
+                  <Button color="primary" variant="contained" type="submit">
+                    Submit
+                  </Button>
+                </Grid>
+              </form>
+            </div>
+          </Paper>
+        </Grid>
       </>
     );
   }
--- src/components/Navigationbar/Navbar.jsx
+++ src/components/Navigationbar/Navbar.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import {Toolbar, AppBar, Typography} from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import { IconButton } from '@material-ui/core';
+import React from "react";
+import { Toolbar, AppBar, Typography } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import { IconButton } from "@material-ui/core";
 class Navbar extends React.Component {
-    render() {
-        return(
-                    <AppBar>
-                        <Toolbar>
-                            <IconButton>
-                            <MenuIcon style={{marginRight:5}}></MenuIcon> 
-                            </IconButton>
-                            <Typography>
-                              Mask Pehno
-                            </Typography>
-                        </Toolbar>
-                    </AppBar>
-        );
-    }
+  render() {
+    return (
+      <AppBar>
+        <Toolbar>
+          <IconButton>
+            <MenuIcon style={{ marginRight: 5 }}></MenuIcon>
+          </IconButton>
+          <Typography>Mask Pehno</Typography>
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
 export default Navbar;
