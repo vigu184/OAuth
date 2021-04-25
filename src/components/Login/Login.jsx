56386172ed683b27881657b66d2d4bbabe208579
vigu184 <vigneshiyer666666@gmail.com>
2021-04-26 01:11:22 +0530

    Password validation check done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -13,6 +13,10 @@ const EmailRegex = RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,10}$/i);
 const InvalidPasswordRegex = RegExp(
   /^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$/
 );
+const UppercaseRegex = RegExp(/[A-Z]/);
+const LowerCaseRegex = RegExp(/[a-z]/);
+const NumberRegex = RegExp(/\d/);
+const SpecialCharacter = RegExp(/[@_!#$%^&*()<>?/|}{~:]/);
 class Login extends React.Component {
   constructor() {
     super();
@@ -23,6 +27,7 @@ class Login extends React.Component {
         email: "",
         password: "",
       },
+      passwordStrength: "",
     };
   }
 
@@ -37,10 +42,40 @@ class Login extends React.Component {
         break;
       case "password":
         errors.password = InvalidPasswordRegex.test(value)
-          ? alert(
-              "Password must contain minimum 8 characters with at least 1 Uppercase, 1 Lowercase, 1 Number and 1 special character"
-            )
+          ? "Password must contain minimum 8 characters with at least 1 Uppercase, 1 Lowercase, 1 Number and 1 special character"
           : "";
+        if (value === "") {
+          this.setState({ passwordStrength: "Password cannot be empty" });
+        } else if (value.length < 8) {
+          console.log("Password Must have atleast 8 characters");
+          alert(
+            "Password must contain:\nMinimum 8 characters\n1 Uppercase Letter\n1 Lowercase letter\n1 number\n1 Special character"
+          );
+        } else if (!NumberRegex.test(value)) {
+          console.log("Password Must have atleast one number");
+          this.setState({
+            passwordStrength:
+              "Password must have atleast 1 numerical character",
+          });
+        } else if (!UppercaseRegex.test(value)) {
+          console.log("Password Must have atleast one Uppercase character");
+          this.setState({
+            passwordStrength: "Password must have atleast 1 Uppercase letter",
+          });
+        } else if (!LowerCaseRegex.test(value)) {
+          console.log("Password Must have atleast one lowercase character");
+          this.setState({
+            passwordStrength: "Password must have atleast 1 Lowercase letter",
+          });
+        } else if (!SpecialCharacter.test(value)) {
+          console.log("Password Must have atleast 1 Special character");
+          this.setState({
+            passwordStrength:
+              "Add special characters to make the best possible!",
+          });
+        } else {
+          this.setState({ passwordStrength: "Strong Password" });
+        }
         console.log(errors);
         break;
       default:
@@ -85,10 +120,9 @@ class Login extends React.Component {
                     variant="outlined"
                     value={this.state.email}
                     onChange={this.onChange}
-                    onBlur={this.validate}
                   />
                 </Grid>
-                <Grid style={{ paddingTop: 10 }}>
+                <Grid style={{ paddingTop: 10, paddingBottom: 10 }}>
                   <TextField
                     name="password"
                     label="Enter Password"
@@ -99,8 +133,14 @@ class Login extends React.Component {
                     onBlur={this.validate}
                   />
                 </Grid>
+                <Paper component={Grid}>{this.state.passwordStrength}</Paper>
                 <Grid style={{ paddingTop: 10 }} container justify="center">
-                  <Button color="primary" variant="contained" type="submit">
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    type="submit"
+                    onClick={this.validate}
+                  >
                     Submit
                   </Button>
                 </Grid>
